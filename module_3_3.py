ebc1169b235185f390cc07caf49b28ddbc905467
Anatolius33 <84564302+Anatolius33@users.noreply.github.com>
2024-10-16 18:20:48 +0300

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -1,8 +1,26 @@
-def print_params(*args):
+def print_params(txt, *args, **kwargs):
+    # Выводим текстовый параметр
+    print(txt)
+    
+    # Выводим аргументы из списка
     for item in args:
         print(item, end=' ')
+    
+    # Выводим аргументы из словаря
+    for key, value in kwargs.items():
+        print(f"{key}: {value}", end=' ')
+    
     print()  # Переход на новую строку после вывода
 
-# Исходный код
-values_list_2 = [54.32, 'Строка']
-print_params(*values_list_2, 42)
+# Создаём список с тремя элементами разных типов
+values_list = [54.32, 'Строка', True]
+
+# Создаём словарь с тремя ключами и значениями разных типов
+values_dict = {
+    'число': 100,
+    'фраза': 'Пример',
+    'логическое': False
+}
+
+# Передаём значения в функцию print_params
+print_params('Список параметров:', *values_list, **values_dict)
